20bc0b23ab5a300b6e6d9ffb6648fba851257a63
Bimal Sunil <bimalsunilofficial.com>
2025-07-26 18:11:39 +0530

    vite comfig updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "assets/css/[name]-style[extname]";
+          }
+          return "assets/[name][extname]";
+        },
+        entryFileNames: "assets/js/[name].js",
+        chunkFileNames: "assets/js/[name].js",
+      },
+    },
+  },
 });
